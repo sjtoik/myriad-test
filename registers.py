9672d8d05c76576da96cf4838cce9d24de9dffe1
Santeri Toikka <santeri.toikka@elisa.fi>
2019-02-10 16:19:10 +0200

    Reading Soapy

--- registers.py
+++ registers.py
@@ -1,19 +1,57 @@
+import json
+import enum
 import SoapySDR
-from pyLMS7002Soapy import LMS7002
-from pyLMS7002Soapy.pyLMS7002Soapy import pyLMS7002Soapy
+import collections
+
+from pyLMS7002Soapy import pyLMS7002Soapy
+
+
+def print_low_level(sdr):
+    reg_banks = sdr.LMS7002.regDesc.getRegBanks()
+    for bank in reg_banks:
+        for register in bank.registers:
+            print(f'{bank.name}: {register.name}')
+
+
+def get_info(sdr):
+    """ pyLMS.sdr Soapy driver documentation:
+    https://pothosware.github.io/SoapySDR/doxygen/latest/classSoapySDR_1_1Device.html """
+
+
+class Direction(int, enum.Enum):
+    TX = SoapySDR.SOAPY_SDR_TX
+    RX = SoapySDR.SOAPY_SDR_RX
 
 
 if __name__ == '__main__':
-    sdr = pyLMS7002Soapy(0)
 
-    registers = sdr.LMS7002.regDesc.regNameDict
-    for key, value in registers.items():
-        print(key)
+    pyLMS = pyLMS7002Soapy.pyLMS7002Soapy(verbose=True)
+    banks = pyLMS.LMS7002.getRegisterBanks()
 
-    mSPI_DFM = sdr.LMS7002.regDesc.getRegistersByName(['mSPI_DFM'])
-    mSPI_DFM_address = sdr.LMS7002.regDesc.getRegisterAddresesByName(['mSPI_DFM'])
-    print('mSPI_DFM:')
-    print(mSPI_DFM)
-    print('mSPI_DFM_address:')
-    print(mSPI_DFM_address)
+    for bank in banks:
+        print(bank)
 
+    info = {
+        'clock_sources': pyLMS.sdr.listClockSources(),
+        'GPIO_banks': pyLMS.sdr.listGPIOBanks(),
+        'register_interfaces': pyLMS.sdr.listRegisterInterfaces(),
+        'time_sources': pyLMS.sdr.listTimeSources(),
+        'sensors': pyLMS.sdr.listSensors(),
+        'UARTs': pyLMS.sdr.listUARTs(),
+    }
+
+    for direction in Direction:
+        channel_count = pyLMS.sdr.getNumChannels(direction.value)
+        info[direction.name] = {}
+
+        for channel in range(channel_count):
+            info[direction.name][channel] = {
+                'tunable_elements': pyLMS.sdr.listFrequencies(direction, channel),
+                'sample_rates': pyLMS.sdr.listSampleRates(direction, channel),
+                'gains': pyLMS.sdr.listGains(direction, channel),
+                'frequencies': pyLMS.sdr.listFrequencies(direction, channel),
+                'bandwidths': pyLMS.sdr.listBandwidths(direction, channel),
+                'antennas': pyLMS.sdr.listAntennas(direction, channel),
+            }
+
+    print(json.dumps(info, indent=4))
